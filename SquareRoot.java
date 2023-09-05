1574127e622a9eb578d58526d2b238e095791fbe
sayantee98 <64141367+sayantee98@users.noreply.github.com>
2023-09-06 03:27:58 +0530

    Update SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,4 +1,5 @@
-///WAP to find whether a number is a perfect square or not.
+///WAP to find whether a number is a perfect square or not, if perfect then return the value else return the floor value.
+//https://practice.geeksforgeeks.org/problems/square-root/1?page=1&category[]=Binary%20Search&sortBy=submissions
 import java.util.Scanner;
 public class SquareRoot {
     public static void main(String[] args){
